ea4201fdd2b1b22302c888264e65bd6e50376cd3
indre101 <zygaityte.indre@gmail.com>
2020-03-07 13:49:21 +0100

    renamedVariable

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-document.querySelector("#circle").addEventListener("mousemove", moveCircle)
+const magnifyIngGlass = document.querySelector("#circle");
+const clipCircle = document.querySelector("#clipcircle");
+const clippedImage = document.querySelector("#clipped-image");
+
+
+magnifyIngGlass.addEventListener("mousemove", moveCircle)
 
 function moveCircle(event) {
   event.target.setAttribute("cx", event.pageX / window.innerWidth * 1000);
   event.target.setAttribute("cy", event.pageY / window.innerHeight * 1000 * (window.innerHeight / window.innerWidth));
-  document.querySelector("#clipcircle").setAttribute("cx", event.pageX / window.innerWidth * 1000);
-  document.querySelector("#clipcircle").setAttribute("cy", event.pageY / window.innerHeight * 1000 * (window.innerHeight / window.innerWidth));
+  clipCircle.setAttribute("cx", event.pageX / window.innerWidth * 1000);
+  clipCircle.setAttribute("cy", event.pageY / window.innerHeight * 1000 * (window.innerHeight / window.innerWidth));
 
-  let newCx = event.pageX / window.innerWidth;
-  let newCY = event.pageY / window.innerHeight
-  if (newCx * 1000 < 550 && newCY * 1000 < 300) {
-    document.querySelector("#clipped-image").setAttribute("x", newCx += 220);
-    document.querySelector("#clipped-image").setAttribute("y", newCY += 120);
+  let coordinateX = event.pageX / window.innerWidth;
+  let coordinateY = event.pageY / window.innerHeight
+  if (coordinateX * 1000 < 550 && coordinateY * 1000 < 300) {
+    clippedImage.setAttribute("x", coordinateX += 220);
+    clippedImage.setAttribute("y", coordinateY += 120);
 
-  } else if (newCx * 1000 < 550 && newCY * 1000 > 300) {
-    document.querySelector("#clipped-image").setAttribute("x", newCx += 220);
-    document.querySelector("#clipped-image").setAttribute("y", newCY -= 120);
+  } else if (coordinateX * 1000 < 550 && coordinateY * 1000 > 300) {
+    clippedImage.setAttribute("x", coordinateX += 220);
+    clippedImage.setAttribute("y", coordinateY -= 120);
 
-  } else if (newCx * 1000 > 550 && newCY * 1000 > 300) {
-    document.querySelector("#clipped-image").setAttribute("x", newCx -= 220);
-    document.querySelector("#clipped-image").setAttribute("y", newCY -= 120);
+  } else if (coordinateX * 1000 > 550 && coordinateY * 1000 > 300) {
+    clippedImage.setAttribute("x", coordinateX -= 220);
+    clippedImage.setAttribute("y", coordinateY -= 120);
 
-  } else if (newCx * 1000 > 550 && newCY * 1000 < 300) {
-    document.querySelector("#clipped-image").setAttribute("x", newCx -= 220);
-    document.querySelector("#clipped-image").setAttribute("y", newCY += 120);
+  } else if (coordinateX * 1000 > 550 && coordinateY * 1000 < 300) {
+    clippedImage.setAttribute("x", coordinateX -= 220);
+    clippedImage.setAttribute("y", coordinateY += 120);
 
   }
-
-
-
-
 }
